839245e7b0176ab6f85b667cfc2f46571f8f8995
Nicolas TAPIA <ntapia.dev@gmail.com>
2022-06-15 17:22:20 +0200

    first commit

---
+++ express/routes/users.js
@@ -0,0 +1,80 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/userlist', function (req, res) {
+  var db = req.db;
+
+  var collection = db.get('userlist');
+  collection.find({}, {}, function (e, docs) {
+    res.render('userlist', {
+      "userlist": docs
+    });
+  });
+});
+
+/* POST add user */
+router.post('/adduser', function (req, res) {
+
+  var db = req.db;
+
+  var userName = req.body.username;
+  var userEmail = req.body.useremail;
+  var userFullName = req.body.userfullname;
+  var userAge = req.body.userage;
+  var userLocation = req.body.userlocation;
+  var userGender = req.body.usergender;
+  var actionType = req.body.actiontype;
+  var updateId = req.body.updateid;
+
+  var collection = db.get('userlist');
+
+  switch (actionType) {
+    case 'edit':
+
+      collection.update({ '_id' : updateId }, {
+        "username": userName,
+        "email": userEmail,
+        "fullname": userFullName,
+        "age": userAge,
+        "location": userLocation,
+        "gender": userGender
+      }, function (err) {
+        if (err) {
+          res.send("Il y a eu un problème pour mettre à jour cet utilisateur");
+        }
+        else {
+          res.redirect("userlist");
+        }
+      });
+      break;
+    case 'add':
+    default:
+      collection.insert({
+        "username": userName,
+        "email": userEmail,
+        "fullname": userFullName,
+        "age": userAge,
+        "location": userLocation,
+        "gender": userGender
+      }, function (err, doc) {
+        if (err) {
+          res.send("Il y a eu un problème pour ajouter cet utilisateur");
+        }
+        else {
+          res.redirect("userlist");
+        }
+      });
+      break;
+    }
+  });
+
+router.get('/:id', function (req, res) {
+  var db = req.db;
+  var userToFind = req.params.id;
+  var collection = db.get('userlist');
+  collection.findOne({ "_id": userToFind }, {}, function (e, docs) {
+    res.json(docs);
+  });
+});
+
+module.exports = router;
